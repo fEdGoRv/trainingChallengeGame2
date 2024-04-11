d6afaabcc12ccda795b0a9fe8e9a8ca8c1d37979
Federico <vilatte612@gmail.com>
2024-04-11 17:05:15 -0300

    applyed setInterval and in case of win

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,24 +1,27 @@
-import {forwardRef, useImperativeHandle, useRef} from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
- const ResultModal= forwardRef(function({ result, targetTime }, ref) {
-
+const ResultModal = forwardRef(function ({ onReset, targetTime, timeRemaining }, ref) {
+    const userLost = timeRemaining <= 0;
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+    const formattedRemainingTime = (timeRemaining / 1000).toFixed(2);
     const dialog = useRef();
 
-    useImperativeHandle(ref, ()=>{
-        return{
-            open(){
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
                 dialog.current.showModal();
-             }
+            }
         }
     })
 
 
 
     return <dialog className="result-modal" ref={dialog} open>
-        <h2>You {result}</h2>
+        {userLost && <h2>You Lost</h2>}
+        {!userLost && <h2>Your score is {score}</h2>}
         <p>The target time was <strong>{targetTime}</strong> seconds.</p>
-        <p>You stopped the timer with<strong> X seconds left.</strong></p>
-        <form method="dialog">
+        <p>You stopped the timer with<strong> {formattedRemainingTime} seconds left.</strong></p>
+        <form method="dialog" onSubmit={onReset}>
             <button>Close</button>
         </form>
     </dialog>
